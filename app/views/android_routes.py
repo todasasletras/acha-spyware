1722eb91691c66e92777bf4d9efa2edd90d428ef
JonasChristiano <jonaschristianoti@gmail.com>
2025-01-22 10:48:25 -0300

    feat: add new routes for listing devices, downloading IOCs, and Downloading APKs
    - Added '/devices' route to list connected devices using the Device model.
    - added '/download-iocs' route to update indicators of compromise (IOCs).
    - Added '/download-apks' route to download APKs with optional analysis.
    
    These changes expand the Android-related API capapilities, aligning with the new features implemented in the MVTController.

--- app/views/android_routes.py
+++ app/views/android_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from app.controllers.mvt_controller import MVTController
+from app.models.device import Device
 
 bp = Blueprint('android_routes', __name__)
 """
@@ -55,3 +56,57 @@ def check_apk():
     output_dir = data.get('output_dir', '/mnt/c/output')
     result = MVTController.check_apk(file_path, output_dir)
     return jsonify(result)
+
+@bp.route('/download-iocs', methods=['POST '])
+def download_iocs():
+    """
+    Endpoit to download indicators of compromise (IOCs).
+
+    Returns
+    -------
+    Response
+        JSON object containing:
+        - `success` (bool): Indicates if the operation was successful.
+        - 'stdout' (str, optional): Standard output if the operation succeeds.
+        - 'stderr' (str, optional): Standard error if the operation fails.
+    """
+    result = MVTController.download_iocs()
+    return jsonify(result)
+
+@bp.route('/download-apks', methods=['POST'])
+def download_apks():
+    """
+    Endpoit to download indicators of compromise (IOCs).
+
+    Expects a JSON payload with:
+    - 'output_dir': Directory for saving the downloaded APKs (optional, defaults to '/mnt/c/output').
+    - 'analyze': Boolean flag to determine if the APKs should be analyzed (optional, defaults to False).
+
+    Returns
+    -------
+    Response
+        JSON object containing:
+        - `success` (bool): Indicates if the operation was successful.
+        - 'stdout' (str, optional): Standard output if the operation succeeds.
+        - 'stderr' (str, optional): Standard error if the operation fails.
+    """
+    data = request.json
+    output_dir = data.get('output_dir', '/mnt/c/output')
+    analyze = data.get('analyze', False)
+    result = MVTController.download_apks(output_dir, analyze)
+    return jsonify(result)
+
+@bp.route('/devices', methods=['GET'])
+def list_devices():
+    """Endpoint to list connected devices.
+
+    Retrieves a list of devices currently connected via ADB, using the Device model.
+
+    Returns
+    -------
+    Response
+        JSON array of device objects, where each object contains device details.
+    
+    """
+    devices = Device.list_connected_devices()
+    return jsonify([device.to_dict() for device in devices])

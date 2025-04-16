6748dbe55a52b048a157900937f1047e91e0f196
JonasChristiano <jonaschristianoti@gmail.com>
2025-04-16 10:04:18 -0300

    feat: added type file and message log stored in a JSON

---
+++ api/models/types/types.py
@@ -0,0 +1,43 @@
+from enum import Enum
+from dataclasses import dataclass
+from typing import TypedDict, List
+
+
+class CategoryType(str, Enum):
+    ERROR_ANALYSIS = "Erro na Análise"
+    POSSIBLE_ATTACK = "Possível Invasão"
+    SUSPICIOUS_APP = "Aplicativos Suspeitos"
+    SYSTEM_SECURITY = "Segurança do Sistema"
+    INFORMATION = "Informação"
+
+
+class MessageLogType(TypedDict):
+    pattern: str
+    category: CategoryType
+    message: str
+
+
+class LogStatus(str, Enum):
+    INFO = "info"
+    WARNING = "warning"
+    ERROR = "error"
+    CRITICAL = "critical"
+
+
+class LogEntry(TypedDict):
+    id: int
+    status: LogStatus
+    message: str
+
+
+class MessageEntry(TypedDict):
+    category: CategoryType
+    message: str
+    original_message: str
+
+
+class APIResponse(TypedDict, total=False):
+    success: bool
+    log: List[LogEntry]
+    messages: MessageEntry
+    error: str

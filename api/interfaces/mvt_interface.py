adac44609e2f13d7c18fd56e36dde5ed06530e63
JonasChristiano <jonaschristianoti@gmail.com>
2025-04-18 00:43:53 -0300

    feat(mvt): added parameter in the interface

--- api/interfaces/mvt_interface.py
+++ api/interfaces/mvt_interface.py
@@ -3,10 +3,26 @@ from abc import ABC, abstractmethod
 
 class MVTInterface(ABC):
     @abstractmethod
-    def check_backup(self): ...
+    def check_backup(
+        self,
+        backup_path: str,
+        iocs_files: list = None,
+        output_folder: str = None,
+        fast: bool = False,
+        list_modules: bool = False,
+        non_interactive: bool = False,
+        backup_password: str = None,
+        verbose: bool = False,
+    ): ...
 
     @abstractmethod
-    def check_iocs(self): ...
+    def check_iocs(
+        self,
+        folder: str = None,
+        iocs_files: list = None,
+        list_modules: bool = False,
+        module: str = None,
+    ): ...
 
     @abstractmethod
     def download_iocs(self): ...
@@ -14,16 +30,54 @@ class MVTInterface(ABC):
 
 class MVTAndroidInterface(MVTInterface):
     @abstractmethod
-    def check_adb(self): ...
+    def check_adb(
+        self,
+        serial: str = None,
+        iocs_files: list = None,
+        output_folder: str = None,
+        fast: bool = False,
+        list_modules: bool = False,
+        module: str = None,
+        non_interactive: bool = False,
+        backup_password: str = None,
+        verbose: bool = False,
+    ): ...
 
     @abstractmethod
-    def check_bugreport(self): ...
+    def check_bugreport(
+        self,
+        bugreport_path: str,
+        iocs_files: list = None,
+        output_folder: str = None,
+        list_modules: bool = False,
+        module: str = None,
+        verbose: bool = False,
+    ): ...
 
     @abstractmethod
-    def check_androidqf(self): ...
+    def check_androidqf(
+        self,
+        androidqf_path: str,
+        iocs_files: list = None,
+        output_folder: str = None,
+        list_modules: bool = False,
+        module: str = None,
+        hashes: bool = False,
+        non_interactive: bool = False,
+        backup_password: str = None,
+        verbose: bool = False,
+    ): ...
 
     @abstractmethod
-    def download_apks(self): ...
+    def download_apks(
+        self,
+        serial: str = None,
+        all_apks: bool = False,
+        virustotal: bool = False,
+        output_folder: str = None,
+        from_file: str = None,
+        verbose: bool = False,
+    ): ...
 
 
 class MVTIOSInterface(MVTInterface): ...

f076739cbd660d3d69ba7a0d6e2654e8a012c2be
JonasChristiano <jonaschristianoti@gmail.com>
2025-04-23 17:42:45 -0300

    refac: better separation of api route and log

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,12 @@
-from .config import logger
-
 from flask import Flask
 
+from core.logger import setup_logger
+from api.controllers.middleware import enforce_json_api
 from api.views import android_routes
 from api.views.views import bp as views_bp
-from api.controllers.mvt_controller import android_bp
-from api.controllers.config_controller import config_bp
+from api.controllers import api_bp
 
+logger = setup_logger()
 
 logger.debug("Função para definição do Flask.")
 
@@ -15,11 +15,12 @@ def create_app():
     app = Flask(__name__, static_folder="../frontend/static", static_url_path="/static")
 
     logger.debug("Registrando controladores de rotas")
-    app.register_blueprint(android_routes.bp)
+    app.register_blueprint(android_routes.bp)  # remover esse registro futuramente
     # API
-    app.register_blueprint(android_bp)
-    app.register_blueprint(config_bp)
+    app.register_blueprint(api_bp)
     # Frontend
     app.register_blueprint(views_bp)
+
+    enforce_json_api(app)
 
     return app
